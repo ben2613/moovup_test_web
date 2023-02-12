a7bf6b7a3705e030912a2a22c4f291a88e1a508f
ben2613 <sosgyuki@gmail.com>
2023-02-12 10:46:26 +0800

    Add 3 more test cases and enable coverage report for Q1

--- q1/PathFinder.test.js
+++ q1/PathFinder.test.js
@@ -43,3 +43,67 @@ test('unreachable node', () => {
     const ADPaths = Array.from(pathFinder.findAllPaths('A', 'D'))
     expect(ADPaths.length).toBe(0)
 })
+
+test('shortest path', () => {
+    const pathFinder = new PathFinder(['A', 'B', 'C', 'D'], [
+        ['A', 'B'],
+        ['B', 'C'],
+        ['C', 'D'],
+        ['A', 'D']
+    ])
+    expect(pathFinder.findShortest('A', 'D')).toStrictEqual(['A', 'D'])
+    // to cover testing the prev.length < curr.length ? prev : curr 
+    const pathFinder2 = new PathFinder(['A', 'B', 'C', 'D'], [
+        ['A', 'D'],
+        ['A', 'B'],
+        ['B', 'C'],
+        ['C', 'D'],
+    ])
+    expect(pathFinder2.findShortest('A', 'D')).toStrictEqual(['A', 'D'])
+})
+
+test('circulating network', () => {
+    const pathFinder = new PathFinder(['A', 'B', 'C', 'D'], [
+        ['A', 'B'],
+        ['B', 'C'],
+        ['C', 'D'],
+        ['D', 'A']
+    ])
+    const ADPaths = Array.from(pathFinder.findAllPaths('A', 'D'))
+    expect(ADPaths.length).toBe(2)
+})
+
+// generate a binary-tree-like network
+test('tree like structure', () => {
+    // they are named as N, N0, N1, N00, N01, N10, N11, 
+    // should be only single path from every pairs of nodes
+    const DEPTH = 5
+    const root = 'N'
+    const nodes = [root]
+    const depthMap = [[root]]
+    const connections = []
+    for (let d = 0; d < DEPTH; d++) {
+        const parentNodes = depthMap[d]
+        depthMap.push([])
+        for (const p of parentNodes) {
+            let child0 = p + '0', child1 = p + '1'
+            nodes.push(child0)
+            nodes.push(child1)
+            connections.push([p, child0])
+            connections.push([p, child1])
+            depthMap[d + 1].push(child0)
+            depthMap[d + 1].push(child1)
+        }
+    }
+    const pathFinder = new PathFinder(nodes, connections)
+    // test all nodes connections
+    for (const start of nodes) {
+        for (const end of nodes) {
+            if (start === end) {
+                continue
+            }
+            let paths = Array.from(pathFinder.findAllPaths(start, end))
+            expect(paths.length).toBe(1)
+        }
+    }
+})
